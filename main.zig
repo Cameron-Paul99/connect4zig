349a55a6f18a13c631eb265328172755760b4629
Cameron Paul <cbp12231999@gmail.com>
2025-11-16 15:42:49 -0600

    Win checker

--- main.zig
+++ main.zig
@@ -24,6 +24,9 @@ const Game = struct {
     red: u64,
     yellow: u64,
     moves: u8,
+    boardHeight: u6,
+    boardWidth: u6,
+    gameOver: bool,
 };
 
 // Overall Game Flow
@@ -70,8 +73,8 @@ pub fn main() !void{
         \\Cameron Paul
     ;
 
-    const WIDTH = 6;
-    const HEIGHT = 7;
+    const WIDTH = 7;
+    const HEIGHT = 6;
 
     const ns_per_frame = std.time.ns_per_ms * 16;
 
@@ -79,6 +82,9 @@ pub fn main() !void{
         .red = 0,
         .yellow = 0,
         .moves = 0,
+        .boardWidth = WIDTH,
+        .boardHeight = HEIGHT,
+        .gameOver = false,
     };
 
     var gameOver = false;
@@ -95,51 +101,108 @@ pub fn main() !void{
 
     try stdout.flush();
 
-    while (!gameOver){
+    while (!game.gameOver){
         
         if ((game.moves & 1) == 0){
             
-            try stdout.writeAll("Place chip in column: ");
+            try stdout.writeAll("Enter a chip in column: ");
             try stdout.flush();
 
             const input = try stdin.takeDelimiterExclusive('\n'); // TODO: Change to both new line and space
             const val = try std.fmt.parseInt(u3, input, 10);
             
-            try stdout.print("\nPlacing chip in column {}\n", .{val});
+            
             try stdout.flush();
-            if (val >= 1 and val <= 5){
+            if (val >= 0 and val <= 6){
 
                 _ = Play(&game, val);
+                _ = try stdin.discardDelimiterInclusive('\n');
 
             }else {
 
                 break; // TODO: redo input if invalid 
             }
-            
-            gameOver = true;
+
+             if (game.gameOver) {
+                try stdout.writeAll("Red wins!!\n");
+                try stdout.flush(); 
+             }
+           
+           // gameOver = true;
             
         }else{
 
+            try stdout.writeAll("Enter a chip in column: ");
+            try stdout.flush();
+            //game.gameOver = true;
+             const input = try stdin.takeDelimiterExclusive('\n'); // TODO: Change to both new line and space
+                                                                   //
+             //if (input.len == 0) {
+              //  try stdout.writeAll("\nEnter a chip\n");
+              //  try stdout.flush();
+             //   continue;
+            // }
+            const val = try std.fmt.parseInt(u3, input, 10);
+          //  const val = std.fmt.parseInt(u3, trimmed, 10) catch |err| switch (err) {
+           //     error.InvalidCharacter => {
+             //       try stdout.writeAll("That is not a valid number.\n");
+               //     try stdout.flush();
+                //    continue; // ask again
+               // },
+               // else => return err, // real I/O/other errors bubble up
+            //};
+
+
+             _ = Play(&game, val); 
+             _ = try stdin.discardDelimiterInclusive('\n');
+             if (game.gameOver) {
+                try stdout.writeAll("Yellow wins!! \n");
+                try stdout.flush(); 
+             }
             //TODO: AI play
 
         }
 
         // TODO: Check if Player or AI has won
-        PrintBoard(game, HEIGHT, WIDTH);
+        //
+        try PrintBoard(game, HEIGHT, WIDTH, stdout);
 
     }else{
-        try stdout.writeAll("\nGame Over \n");
+        try stdout.writeAll("Game Over \n");
         try stdout.flush();
        
         
     }
 }
 
-fn PrintBoard(g: Game, HEIGHT: u8, WIDTH: u8) void{ //TODO: Change to new stdout
+fn BoardCheck(board: u64, height: u6) bool {
+
+    // Vertical Check
+    var m = board & (board >> 1);
+    if ((m & (m >> 2)) != 0) return true;
+
+    // Horizontal Check
+    m = board & (board >> height);
+    if ((m & (m >> height * 2)) != 0) return true;
+
+    // Diagonal Down
+    m = board & (board >> (height + 1));
+    if ((m & (m >> (height + 1) * 2)) != 0) return true;
+
+    // Diagonal Up
+    m = board & (board >> (height - 1));
+    if ((m & (m >> (height - 1) * 2)) != 0) return true;
+
+    return false;
+
+}
+
+fn PrintBoard(g: Game, HEIGHT: u8, WIDTH: u8, writer: *std.Io.Writer) !void{ //TODO: Change to new stdout
 
     for (0..HEIGHT) |rev_row| {
         const row = HEIGHT - 1 - rev_row;
-        print("|", .{});
+        try writer.writeAll("|");
+        try writer.flush();
         for (0..WIDTH) |col|{
             const bit_index = col * 7 + row;
             const bit_mask: u64 = @as(u64, 1) << @intCast(bit_index);
@@ -150,14 +213,15 @@ fn PrintBoard(g: Game, HEIGHT: u8, WIDTH: u8) void{ //TODO: Change to new stdout
             if ((g.red & bit_mask) != 0) cell = 1;
             if ((g.yellow & bit_mask) != 0) cell = 2;
 
-            print("{any} ", .{cell});
+            try writer.print("{any} ", .{cell});
+            try writer.flush();
 
         }
-        print("|\n", .{});
+       try writer.writeAll("|\n");
+       try writer.flush();
     }
 
 }
-
 fn colMask(col: u3) u64 { return (@as(u64, 0x3F) << (@as(u6, col) * 7)); }
 fn bottomMask(col: u3) u64 { return @as(u64, 1) << (@as(u6, col) * 7); }
 
@@ -168,11 +232,16 @@ fn Play(g: *Game, col: u3) bool {
     if ((mask_all & (col_mask << 1)) == col_mask) return false; // column full
 
     const move_bit = (mask_all + bottomMask(col)) & col_mask;
-    if ((g.moves & 1) == 0)
-        g.red ^= move_bit
-    else
-        g.yellow ^= move_bit;
+    if ((g.moves & 1) == 0){
+        g.red ^= move_bit;
+        g.gameOver = BoardCheck(g.red, g.boardHeight);
 
+    }
+    else {
+        g.yellow ^= move_bit;
+        g.gameOver = BoardCheck(g.yellow, g.boardHeight);
+    }
+    
     g.moves += 1;
     return true;
 }
